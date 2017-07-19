96595be020aa52618095187e50d62d718becf189
unknown <nskuza@ECONDW7-039912.eastern.ewu.edu>
2017-07-19 15:33:14 -0700

    see if you can find the change

--- Housing_Data.R
+++ Housing_Data.R
@@ -19,6 +19,7 @@ library(dplyr)
 library(data.table)
 library(hms)
 
+# this change is for practice #
 
 #### Reading in Original Shapefile of LIHTC Housing Projects from HUD's geospatial data website ####
 LIHTC <- readOGR(dsn = "C:/Users/cwonderly/Downloads/LIHTC", layer = "LowIncome_Housing_Tax_Credit_Properties", stringsAsFactors = FALSE)
