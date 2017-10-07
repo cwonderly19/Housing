523bf207baf2d95fcb1184691e2e2321246756aa
Coral <wonderly.19@buckeyemail.osu.edu>
2017-10-06 20:12:14 -0400

    Balaned

--- Dataset_Creation.R
+++ Dataset_Creation.R
@@ -55,15 +55,17 @@ levels_approx$Year <- as.numeric(levels_approx$Year)
 levels_approx$GEOID <- as.numeric(levels_approx$GEOID)
 levels_approx$Level <- as.numeric(levels_approx$Level)
 # Merging back with original data
-Synth_Practice <- merge(Synth_Practice, levels_approx, by = c("Year", "GEOID"))
+Synth_Practice <- merge(Synth_Practice, levels_approx, by = c("Year", "GEOID"), all = FALSE)
 
 ##### Creating All_Housing Variable ####
 practice <- Synth_Practice
 practice$Placement = (practice$P_02 + practice$P_03 + practice$P_04 + practice$P_05 + practice$P_06 + practice$P_07 + 
                         practice$P_08 + practice$P_09 + practice$P_10 + practice$P_11 + practice$P_12 + practice$P_13 + 
                         practice$P_14 +  + practice$P_15)
+practice <- practice[-c(27)]
 
-practice_dcast <- dcast(practice, practice$GEOID ~ practice$Year, value.var = "Placement")
+practice_dcast <- dcast(practice, practice$GEOID ~ practice$Year, value.var = "Placement", fun.aggregate = sum)
+practice_dcast[is.na(practice_dcast)] <- 0
 
 practice_dcast$'2003' <- ifelse(practice_dcast$'2002'  == 1, 1, practice_dcast$'2003')
 practice_dcast$'2004' <- ifelse(practice_dcast$'2003'  == 1, 1, practice_dcast$'2004')
@@ -82,14 +84,14 @@ practice_dcast$'2015' <- ifelse(practice_dcast$'2014'  == 1, 1, practice_dcast$'
 practice<- melt(practice_dcast, id.vars = "practice$GEOID", value.name = "All_Housing", variable.name = "Year")
 
 colnames(practice)[1] <- "GEOID"
-Synth_Practice <- merge(Synth_Practice, practice, by = c("GEOID", "Year"))
+Synth_Practice <- merge(Synth_Practice, practice, by = c("GEOID", "Year"), all = FALSE)
 Synth_Data <- Synth_Practice[-c(4:21)]
 colnames(Synth_Data)[9] <- "Test_Level"
 colnames(Synth_Data)[10] <- "Treated"
 
 Synth_Data$Treatment <- ifelse(Synth_Data$Units >= 1,1,0)
-Synth_Data$Control <- ifelse(Synth_Data$Treatment == 0 & Synth_Data$All_Housing == 0, 1,0)
-Synth_Data <- make_balanced(Synth_Practice)
+Synth_Data$Control <- ifelse(Synth_Data$Treatment == 0 & Synth_Data$Treated == 0, 1,0)
+
 
 post_treatment_dcast <- dcast(Synth_Data, Synth_Data$GEOID ~ Synth_Data$Year, value.var = "Treated")
 post_treatment_dcast$'2015_2' <- ifelse(post_treatment_dcast$'2015'  == 1, 0, 0)
@@ -143,10 +145,20 @@ pre_treat <- melt(post_treatment_dcast, id.vars = "Synth_Data$GEOID", value.name
 colnames(pre_treat)[1] <- "GEOID"
 pre_treat$Year <- as.character(pre_treat$Year)
 pre_treat$Year <- as.numeric(pre_treat$Year)
-Synth_Data <- merge(Synth_Data, pre_treat, by = c("GEOID", "Year"))
+Synth_Data <- merge(Synth_Data, pre_treat, by = c("GEOID", "Year"), all = FALSE)
 
 Synth_Data$Control <- ifelse(Synth_Data$Pre_Treat == 0 & Synth_Data$Treated == 0 & Synth_Data$Treatment == 0, 1,0)
 Synth_Data$Post_Treatment <- ifelse(Synth_Data$Treated == 1 & Synth_Data$Treatment == 0, 1,0)
 
 write.csv(Synth_Data, "C:/Users/cwond/Documents/Housing_Project/Synth_Data.csv", row.names = FALSE)
 
+balance <- function(x, years) { 
+  xlist <- split(x, x$GEOID) 
+  new <- list() 
+  dat <- lapply(xlist, function(z) if(length(unique(z$Year)) == years) {new <- z} ) 
+  dat_ <- do.call(rbind, dat) 
+  return(dat_) 
+} 
+Synth_Data <- balance(Synth_Data, 14) 
+
+write.csv(Synth_Data, "C:/Users/cwond/Documents/Housing_Project/Balanced_Synth_Data.csv", row.names = FALSE)
--- Synthethic_Controls_Practice.R
+++ Synthethic_Controls_Practice.R
@@ -12,97 +12,18 @@ library(splitstackshape)
 data(gsynth)
 head(simdata)
 
-#### Creating Data for Synthetic Controls ####
-# If you uploaded district level from Git you will need to remove the attributes at the end
-District_Level <- as.matrix(District_Level)
-District_Level <- as.data.frame(District_Level)
-Synth_Practice <- (District_Level)
-Synth_Practice <- Synth_Practice[c(2,27,3:26)] # Rearranging columns 
-
-# Removing or interpolating N/As within Level varible
-levels <- dcast(Synth_Practice, Synth_Practice$Year ~ Synth_Practice$GEOID, value.var = "Level") #converting to short format
-levels <- levels[-c(16,18,22,26,30,41,53,63,79,80,89,90,96,98)] # If a district had more than 2 years of assessment data missing in a row they were excluded
-levels <- levels[-c(91,93,98)]
-levels <- levels[-c(99,100)]
-levels_2 <- levels[c(1,101:262)]
-levels <- levels[c(1:100)]
-levels_2 <- levels_2[-c(4,15,23,30,38,43,52,54,55,60,63,74,96,99,100)]
-levels_2 <- levels_2[-c(96,97,99)]
-levels_2 <- levels_2[-c(99)]
-levels_3 <- levels_2[c(1,101:144)]
-levels_2 <- levels_2[c(1:100)]
-levels_3 <- levels_3[-c(27,38,41)]
-levels <- cbind(levels, levels_2, levels_3)
-
-# Linear approximation of remaining N/A values, except for any missing from 2002
-levels_approx <- as.data.frame(lapply(levels,
-                             function(x) approx(seq_along(x),x,seq_along(x), method = "linear")$y
-))
-# NOCB (Next observation carried backwards) approximation of any missing 2002 values
-levels_approx <- na.locf(levels_approx, na.rm = FALSE, fromLast = TRUE)
-
-# Converting back to long format
-levels_approx <- melt(levels_approx, id.vars = "Synth_Practice.Year", value.name = "Level", variable.name = "GEOID", na.rm = TRUE)
-levels_approx$GEOID <- as.character(levels_approx$GEOID)
-levels_approx <- cSplit(levels_approx, "GEOID", "X")
-levels_approx <- as.matrix(levels_approx)
-levels_approx <- as.data.frame(levels_approx, stringsAsFactors = FALSE)
-levels_approx <- levels_approx[c(1,4,2)]
-colnames(levels_approx)[c(1)] <- "Year"
-colnames(levels_approx)[c(2)] <- "GEOID"
-colnames(levels_approx)[c(3)] <- "Level"
-levels_approx$Year <- as.numeric(levels_approx$Year)
-levels_approx$GEOID <- as.numeric(levels_approx$GEOID)
-levels_approx$Level <- as.numeric(levels_approx$Level)
-# Merging back with original data
-Synth_Practice <- merge(Synth_Practice, levels_approx, by = c("Year", "GEOID"))
-
-# Repeating again with share variable (see comments above)
-share <- dcast(Synth_Practice, Synth_Practice$Year ~ Synth_Practice$GEOID, value.var = "Share_Meeting_Standard")
-share <- share[-c(13,15,18,24,36,53,65,70,72,77,80,90)]
-share <- share[-c(89,95,99)]
-share <- share[-c(102,104,105,110,118,135,136,146,176,179)]
-
-
-share_approx <- as.data.frame(lapply(share,
-                                      function(x) approx(seq_along(x),x,seq_along(x), method = "linear")$y
-))
-
-share_approx <- na.locf(share_approx, na.rm = FALSE, fromLast = TRUE)
-
-share_approx <- melt(share_approx, id.vars = "Synth_Practice.Year", value.name = "Level", variable.name = "GEOID", na.rm = TRUE)
-share_approx$variable <- as.character(share_approx$variable)
-share_approx <- cSplit(share_approx, "variable", "X")
-share_approx <- as.matrix(share_approx)
-share_approx <- as.data.frame(share_approx)
-share_approx <- share_approx[c(1,4,2)]
-colnames(share_approx)[c(1)] <- "Year"
-colnames(share_approx)[c(2)] <- "GEOID"
-colnames(share_approx)[c(3)] <- "Share_Meetings_Standard"
-
-Synth_Practice <- merge(Synth_Practice, share_approx, by = c("Year", "GEOID"), all = TRUE)
-
-
-
 # Removing any data from early years that had housing placed 
-Synth_Data <- Synth_Practice[!Synth_Practice$Year == 2002 | Synth_Practice$All_Housing != 1,]
-Synth_Data <- Synth_Data[!Synth_Data$Year == 2003 | Synth_Data$All_Housing != 1,]
-Synth_Data <- Synth_Data[!Synth_Data$Year == 2004 | Synth_Data$All_Housing != 1,]
-Synth_Data <- Synth_Data[!Synth_Data$Year == 2005 | Synth_Data$All_Housing != 1,]
-Synth_Data <- Synth_Data[!Synth_Data$Year == 2006 | Synth_Data$All_Housing != 1,]
+
+Synth_Data <- Synth_Data[!(Synth_Data$Year == 2002 & Synth_Data$Treated == 1),]
+Synth_Data <- Synth_Data[!(Synth_Data$Year == 2003 & Synth_Data$Treated == 1),]
+Synth_Data <- Synth_Data[!(Synth_Data$Year == 2004 & Synth_Data$Treated == 1),]
+Synth_Data <- Synth_Data[!(Synth_Data$Year == 2005 & Synth_Data$Treated == 1),]
+Synth_Data <- Synth_Data[!(Synth_Data$Year == 2006 & Synth_Data$Treated == 1),]
 
 # Balancing the data after removing earlier observations
-Synth_Data <- make_balanced(Synth_Data)
+Synth_Data <- balance(Synth_Data, 14)
 
-## Rearranging and cleaning data 
-Synth_Data <- Synth_Data[-c(4,5)]
-Synth_Data <- Synth_Data[c(1,2,29,28,3:27)]
-colnames(Synth_Data)[3] <- "Share_Meeting_Standard"
-colnames(Synth_Data)[4] <- "Level"
-
-## Saving to Git as CSV
-write.csv(Synth_Data, "C:/Users/cwonderly/Documents/Housing/Housing_Education_Project/Synth_Data.csv", row.names = FALSE)
-
+write.csv(Synth_Data, "C:/Users/cwond/Documents/Housing_Project/Restricted_Balanced.csv", row.names = FALSE)
 
 #### Descriptive Statistics ####
 stargazer(Synth_Data, type = "text", title = "Descriptive Statistics", digit.separate = 3, out = "descriptive_stats_2.txt")
@@ -117,7 +38,7 @@ total_treatment <- unique(treatment$GEOID)
 #Synth_Data <- as.data.frame(Synth_Data)
 
 >>>>>>> f6a818d137f9259b2fc60631bf51750239737ca4
-out <- gsynth(Share_Meeting_Standard ~ All_Housing, data = Synth_Data, 
+out <- gsynth(Test_Level ~ Treated, data = Synth_Data, 
               index = c("GEOID","Year"), force = "unit",
               CV = FALSE, r = 0 , se = TRUE, 
               inference = "parametric", nboots = 1000,
